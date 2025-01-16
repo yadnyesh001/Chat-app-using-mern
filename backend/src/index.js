292f445636acc07990e2e1aae25f3f977ab8466e
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-01-16 15:12:21 +0530

    login signup

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import { connectDB } from './lib/db.js';
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log('Server is running on port: ' + PORT);
+  connectDB();
+});
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model';
+
+
+export const protectRoute = async (req, req, next) =>{
+  try {
+    const token = req.cookies.jwt;
+
+    if(!token){
+      return resizeBy.status(401).json({message: "Unauthorized - No token provided"});
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if(!decoded){
+      return res.status(401).json({message: "Unauthorized - Invalid Token"});
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if(!user){
+      return res.status(404).json({message: "user not found"});
+    }
+
+    req.user = user;
+    next();
+
+  } catch (error) {
+    
+  }
+}
